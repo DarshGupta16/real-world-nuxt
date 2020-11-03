ba58c04f221b3c80ff2edbb74b2d59172f8ef1db
Darsh Gupta <dg@darshgupta.com>
2020-11-03 22:57:43 +0530

    Adding EventCard Component and showing all events

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+
 export default {
   head() {
     return {
@@ -16,11 +24,14 @@ export default {
     }
   },
   asyncData({ $axios }) {
-    $axios.get('http://localhost:3000/events').then((res) => {
+    return $axios.get('http://localhost:3000/events').then((res) => {
       return {
         events: res.data,
       }
     })
+  },
+  components: {
+    EventCard,
   },
 }
 </script>
