cddd60eac70b256c3d841f635be93f492eccf31f
Darsh Gupta <dg@darshgupta.com>
2020-11-03 20:19:47 +0530

    Adding some SEO features

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,16 @@
     <h1>Create an Event</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event - Real World Events',
+      hid: 'description',
+      name: 'description',
+      content: 'Create an event for your neighbourhood',
+    }
+  },
+}
+</script>
