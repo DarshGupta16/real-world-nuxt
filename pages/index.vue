f1455efeb74d9578b9e5db8e6ddd5325b03bbac7
Darsh Gupta <dg@darshgupta.com>
2020-11-04 11:07:11 +0530

    Adding a seperate EventService file

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
   head() {
@@ -23,9 +24,9 @@ export default {
         'Where you can find all the events taking place in your neighbourhood',
     }
   },
-  async asyncData({ $axios, error }) {
+  async asyncData({ error }) {
     try {
-      const { data } = await $axios.get('http://localhost:3000/events')
+      const { data } = await EventService.getEvents()
       return {
         events: data,
       }
